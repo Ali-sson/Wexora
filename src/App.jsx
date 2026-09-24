e8f42f96ad9b7b5e2a2dea6918d7c3ccab131ff9
Alisson <alissontech247@gmail.com>
2026-09-25 00:20:41 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+  <Home/>
   );
 }
 
