3b11d35d1d1bcd0c00f329ab296193b12894c61f
Alisson <alissontech247@gmail.com>
2026-09-25 00:52:10 +0100

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,7 @@ function Home() {
   return (
     <>
 
-      <div className="bg-red-500 p-10 text-center text-4xl font-bold text-white">
-        TAILWIND TEST
-      </div>
-      
+    
     <Navbar />
     <Hero />
     <About />
